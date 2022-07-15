399c225d3c08947a9545bbe3c372dfcace1f36c7
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-07-15 18:38:04 -0400

    feat: notifications page

--- src/routes/notifications/index.svelte
+++ src/routes/notifications/index.svelte
@@ -61,15 +61,10 @@
 
 	function bulkRead() {
 		notifications = notifications.map((notification) => {
-			notification.read = true;
+			notification.read = selectedNotifications.includes(notification) ? true : notification.read;
 			return notification;
 		});
 
-		selectedNotifications = [];
-	}
-
-	function bulkDelete() {
-		notifications = [];
 		selectedNotifications = [];
 	}
 </script>
@@ -85,3 +80,70 @@
 		<button class="btn btn-primary w-100"> Send message </button>
 	</div>
 </div>
+
+<div class="table-responsive mt-3">
+	<table class="table table-striped table-bordered">
+		<thead>
+			<tr>
+				<th style="width: 0;" scope="col">
+					<input
+						on:change={toggleAll}
+						{checked}
+						{indeterminate}
+						type="checkbox"
+						class="form-check-input"
+					/>
+				</th>
+				<th scope="col">
+					{#if selectedNotifications.length > 0}
+						<div style="height: auto;" class="d-flex">
+							<button on:click={bulkRead} class="btn btn-sm py-0 btn-outline-primary">
+								Bulk read
+							</button>
+						</div>
+					{:else}
+						Sender
+					{/if}
+				</th>
+				<th scope="col">Title</th>
+				<th scope="col">Date</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each notifications as notification (notification.id)}
+				{@const selected = selectedNotifications.includes(notification)}
+				<tr class:row-selected={selected}>
+					<td class="position-relative" style="width: 0;">
+						{#if !notification.read}
+							<div
+								style="width: 0.125rem;"
+								class="position-absolute top-0 bottom-0 start-0 bg-primary"
+							/>
+						{/if}
+						<input
+							bind:group={selectedNotifications}
+							value={notification}
+							type="checkbox"
+							class="form-check-input"
+						/>
+					</td>
+					<td>
+						{#if !notification.read}
+							<strong>{notification.sender}</strong>
+						{:else}
+							{notification.sender}
+						{/if}
+					</td>
+					<td>{notification.title}</td>
+					<td>{formatDate(notification.date)}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</div>
+
+<style>
+	.row-selected {
+		background-color: rgba(0, 0, 0, 0.15);
+	}
+</style>
